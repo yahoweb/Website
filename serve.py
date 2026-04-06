8e2627c4673e585236784a320e101050375abd40
yahoweb <yoon001005@gmail.com>
2026-04-06 04:46:16 -0400

    Delete serve.py

--- serve.py
+++
@@ -1,8 +0,0 @@
-import http.server
-import os
-
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
-handler = http.server.SimpleHTTPRequestHandler
-httpd = http.server.HTTPServer(("", 3000), handler)
-httpd.serve_forever()
